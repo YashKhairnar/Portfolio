5f58a169c609764b074ca5ef39d17d757f70e26b
Yash <yashkvk7@gmail.com>
2025-01-05 20:04:13 +0530

    changes '

--- src/Components/Bullets.jsx
+++ src/Components/Bullets.jsx
@@ -7,8 +7,8 @@ export default function Bullets(){
         <li className=" text-gray-400 tracking-wide text-3xl font-poppins hover:scale-125 hover:text-white transition-transform "><a href="#work">Work Experience</a></li>
         <br />
         <li className=" text-gray-400 tracking-wide text-3xl font-poppins hover:scale-125 hover:text-white transition-transform "><a href="#techstack">Tech Stack</a></li>
-        <br />
-        <li className=" text-gray-400 tracking-wide text-3xl font-poppins hover:scale-125 hover:text-white transition-transform ">Projects</li>
+        {/* <br /> */}
+        {/* <li className=" text-gray-400 tracking-wide text-3xl font-poppins hover:scale-125 hover:text-white transition-transform ">Projects</li> */}
         <br />
         <li className=" text-gray-400 tracking-wide text-3xl font-poppins hover:scale-125 hover:text-white transition-transform "><a href="#dsa">DSA Journey</a></li>
         </ul>
